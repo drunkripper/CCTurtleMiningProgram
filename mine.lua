a757e6d3171923614d9962865929bdfff348c928
TheFakeJaneDoe <drunkripper@users.noreply.github.com>
2021-02-14 09:00:19 -0500

    Update mine.lua

--- mine.lua
+++ mine.lua
@@ -28,7 +28,7 @@ function mine()
   turtle.place()
   turtle.select(slotFluxPoint)
   turtle.placeDown()
-  sleep(10)
+  sleep(15)
   turtle.digDown()
   turtle.select(slotEmptyChest)
   turtle.dig()
@@ -37,47 +37,72 @@ function mine()
   turtle.dig()
 end
 
-function sendError(error)
-  print(error)
+function sendError(errorMessage)
+  print(errorMessage)
+  modem.open(4)
+  modem.transmit(4, 4, "Miner: " .. os.getComputerID() .. " is missing a " .. errorMessage)
 end
 
 function checkInventoryCorrect()
-  local slotMiningWell = 13
-  local slotFluxPoint = 14
-  local slotFuelChest = 15
-  local slotEmptyChest = 16
-  
-  
+  local result = true
+
+  print("Checking Inventory Setup")
   local _, slotMiningWellName = turtle.getItemDetail(slotMiningWell)
   if slotMiningWellName ~= "quarryplus:miningwellplus" then
-    print("Mining Well not in place. Sending Alert to master Error Server")
 	sendError("Mining Well")
+    result = false
   end
   local _, slotFluxPointName = turtle.getItemDetail(slotFluxPoint)
   if slotFluxPointName ~= "fluxnetworks:flux_point" then
-    print("Flux Point not in place. Sending Alert to master Error Server")
 	sendError("Flux Point")
+    result = false
   end
   local _, slotFuelChestName = turtle.getItemDetail(slotFuelChest)
   if slotFuelChestName ~= "enderstorage:ender_chest" then
-    print("Fuel Chest not in place. Sending Alert to master Error Server")
 	sendError("Fuel Chest")
+    result = false
   end
   local _, slotEmptyChestName = turtle.getItemDetail(slotEmptyChest)
   if slotEmptyChestName ~= "quarryplus:miningwellplus" then
-    print("Empty Chest not in place. Sending Alert to master Error Server")
 	sendError("Empty Chest")
+	result = false
   end
   
+  return result
   
 end
 
 modem.open(2)
+--checkInventoryCorrect()
 while true do
   refuel()
   local event_type, side, recievedChannel, replyChannel, message, distance = os.pullEvent("modem_message")
   if message == "advance" then
     turtle.forward()  
     mine()
+  end
+  if message == "setupMiningWell" then
+    if turtle.getItemCount(slotMiningWell) == 0 then
+	  turtle.select(slotMiningWell)
+	  turtle.suckDown(1)
+	end
+  end
+  if message == "setupFluxPoint" then
+    if turtle.getItemCount(slotFluxPoint) == 0 then
+	  turtle.select(slotFluxPoint)
+	  turtle.suckDown(1)
+	end
+  end
+  if message == "setupFuelChest" then
+    if turtle.getItemCount(slotFuelChest) == 0 then
+	  turtle.select(slotFuelChest)
+	  turtle.suckDown(1)
+	end
+  end
+  if message == "setupEmptyChest" then
+    if turtle.getItemCount(slotEmptyChest) == 0 then
+	  turtle.select(slotEmptyChest)
+	  turtle.suckDown(1)
+	end
   end
 end
