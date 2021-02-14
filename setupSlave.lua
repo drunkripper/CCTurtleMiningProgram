7d8cf44f972ad52a3323b086b86d8d7e2f066173
TheFakeJaneDoe <drunkripper@users.noreply.github.com>
2021-02-14 09:03:44 -0500

    Update and rename update.lua to setupSlave.lua

---
+++ setupSlave.lua
@@ -0,0 +1,2 @@
+shell.run("wget", "https://raw.githubusercontent.com/drunkripper/CCTurtleMiningProgram/main/startup.lua", "startup")
+os.reboot()
--- update.lua
+++
@@ -1,3 +0,0 @@
-shell.run("rm", "mine.lua")
-shell.run("wget", "https://raw.githubusercontent.com/drunkripper/CCTurtleMiningProgram/main/mine.lua", "mine.lua")
-shell.run("mine")
